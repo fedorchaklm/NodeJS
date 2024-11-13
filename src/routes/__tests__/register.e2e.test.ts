ccf70cdc7c426a74048c3eeeaeb39df354f8cf1d
fedorchaklm <fedorchak.luba@gmail.com>
2024-11-13 23:09:15 +0200

    in progress

--- src/routes/__tests__/register.e2e.test.ts
+++ src/routes/__tests__/register.e2e.test.ts
@@ -28,6 +28,16 @@ describe('POST /api/register E2E TESTS', () => {
     expect(res.body.email).toEqual('sun.jones.2@epam.com');
   });
 
+  it('should return "Email or password format is invalid" when password or email is not valid', async () => {
+    const res = await request(app).post('/api/register').send({
+      email: 'sun.jones.2@epam.com',
+      name: 'Mary Smith',
+      password: 'a2A!',
+    });
+    expect(res.statusCode).toEqual(422);
+    expect(res.text).toBe('Email or password format is invalid');
+  });
+
   it('should throw error if user already exists', async () => {
     await request(app).post('/api/register').send({
       email: 'love.jones.2@epam.com',
