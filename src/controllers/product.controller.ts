8c4d4b3b268990c2c135757d991170742f8165b6
fedorchaklm <fedorchak.luba@gmail.com>
2024-11-14 22:21:09 +0200

    in progress

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import { Product } from 'src/types/types';
 import * as productService from '../services/product.service';
 import { NextFunction, Request, Response } from 'express';
+import { NotFoundError } from '../common/errors';
 
 export const getAllProducts = async (_: Request, res: Response<Array<Product>>, next: NextFunction) => {
   try {
@@ -16,7 +17,7 @@ export const getProductById = async (req: Request, res: Response<Product>, next:
     const productId = req.params.productId;
     const product = await productService.getProductById(productId);
     if (product == null) {
-      throw Error();
+      throw new NotFoundError();
     }
     res.status(200).json(product);
   } catch (e) {
--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -4,13 +4,5 @@ import * as loginController from "../controllers/login.controller";
 const router = Router();
 
 router.post("/", loginController.login);
-/**
- * @swagger
- * /api/register:
- *   post:
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
- */
+
 export default router;
